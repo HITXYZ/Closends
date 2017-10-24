316ede4b21fc0057803063bfbf582d05cf9518e1
bluestyle97 <690147660@qq.com>
2017-10-24 14:41:17 +0800

    :bug:添加微博异常类

---
+++ weibo/exception.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+"""
+    @author: Jiale Xu
+    @date: 2017/10/23
+    @desc: Exceptions of weibo scraping
+"""
+
+
+class WeiboError(Exception):
+    pass
+
+
+class LoginError(WeiboError):
+    def __init__(self, value="Login failed!"):
+        self.info = value
+
+    def __str__(self):
+        return repr(self.info)
+
+
+class ItemAttrError(WeiboError):
+    def __init__(self, attr):
+        self.info = "Attribute %s is illegal!" % attr
+
+    def __str__(self):
+        return repr(self.info)
+
+
+class ItemEmptyError(WeiboError):
+    def __init__(self, value="This item is invalid since its user id hasn't been initialized!"):
+        self.info = value
+
+    def __str__(self):
+        return self.info
