d8e3e132ee0c766bc5537b7eb795f743db82690f
bluestyle97 <690147660@qq.com>
2017-10-25 21:51:43 +0800

    :white_check_mark:添加微博条目

--- weibo/items.py
+++ weibo/items.py
@@ -8,19 +8,28 @@ from exception import ItemAttrError, ItemEmptyError
 
 
 class WeiboUserItem:
-    fields = ["user_id", "user_name", "sex", "address", "birthday", "synopsis",
+    _fields = ["user_id", "user_name", "sex", "address", "birthday", "synopsis",
               "weibo_number", "follow_number", "fans_number"]
 
     def __init__(self):
         self._value = {}
+        self._value["user_id"] = None
+        self._value["user_name"] = None
+        self._value["sex"] = None
+        self._value["address"] = None
+        self._value["birthday"] = None
+        self._value["synopsis"] = None
+        self._value["weibo_number"] = 0
+        self._value["follow_number"] = 0
+        self.fans_number = 0
 
     def __getitem__(self, key):
-        if key not in self.fields:
+        if key not in self._fields:
             raise ItemAttrError(key)
         return self._value[key]
 
     def __setitem__(self, key, value):
-        if key not in self.fields:
+        if key not in self._fields:
             raise ItemAttrError(key)
         else:
             self._value[key] = value
@@ -42,6 +51,43 @@ class WeiboUserItem:
         return string
 
     def __hash__(self):
-        if "user_id" in self._value.keys():
-            return hash(self._value["user_id"])
-        raise ItemEmptyError()
+        if "user_id" not in self._value.keys():
+            raise ItemEmptyError()
+        return hash(self._value["user_id"])
+
+
+class WeiboItem:
+    _fields = ["from", "time", "content", "images"]
+
+    def __init__(self):
+        self._value = {}
+        self._value["from"] = None
+        self._value["time"] = None
+        self._value["content"] = None
+        self._value["images"] = []
+
+    def __getitem__(self, key):
+        if key not in self._fields:
+            raise ItemAttrError(key)
+        return self._value[key]
+
+    def __setitem__(self, key, value):
+        if key not in self._fields:
+            raise ItemAttrError(key)
+        else:
+            self._value[key] = value
+
+    def __str__(self):
+        if "content" not in self._value.keys():
+            raise ItemEmptyError()
+        string = "Content: " + str(self._value["content"]) + "\n"
+        if "from" in self._value.keys():
+            string += "From: " + str(self._value["from"]) + "\n"
+        if "time" in self._value.keys():
+            string += "Time: " + str(self._value["time"]) + "\n"
+        return string
+
+    def __hash__(self):
+        if "content" not in self._value.keys():
+            raise ItemEmptyError()
+        return hash(self._value["content"])
