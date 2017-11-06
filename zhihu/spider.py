c943ecf3901acc4c208995d5189206b3bbf5b2a7
bluestyle97 <690147660@qq.com>
2017-11-06 17:11:17 +0800

    :rocket:完成知乎问题和答案的各种抓取形式

--- zhihu/spider.py
+++ zhihu/spider.py
@@ -24,7 +24,10 @@ user_url = 'https://www.zhihu.com/api/v4/members/{user}?include={include}'
 follows_url = 'https://www.zhihu.com/api/v4/members/{user}/followees?include={include}&offset={offset}&limit={limit}'
 followers_url = 'https://www.zhihu.com/api/v4/members/{user}/followers?include={include}&offset={offset}&limit={limit}'
 question_url = 'https://www.zhihu.com/api/v4/questions/{id}?include={include}'
+user_questions_url = 'https://www.zhihu.com/api/v4/members/{user}/questions?offset={offset}&limit={limit}'
 answer_url = 'https://www.zhihu.com/api/v4/answers/{id}?include={include}'
+user_answers_url = 'https://www.zhihu.com/api/v4/members/{user}/answers?offset={offset}&limit={limit}'
+question_answers_url = 'https://www.zhihu.com/api/v4/questions/{id}/answers?offset={offset}&limit={limit}'
 
 user_query = 'locations,employments,gender,educations,business,voteup_count,thanked_Count,follower_count,' \
              'following_count,following_topic_count,following_question_count,following_favlists_count,' \
@@ -42,11 +45,14 @@ logging.basicConfig(filename=log_file, format="%(asctime)s - %(name)s - %(leveln
 
 class ZhihuSpider:
     def __init__(self):
-        self.info = None
-        self.follows = None
-        self.followers = None
-        self.follows_user = None
-        self.followers_user = None
+        self.scraped_infos = {}
+        self.scraped_follows = {}
+        self.scraped_followers = {}
+        self.scraped_questions = {}
+        self.scraped_user_questions = {}
+        self.scraped_answers = {}
+        self.scraped_question_answers = {}
+        self.scraped_user_answers = {}
 
     def scrape_info(self, user=None):
         if user is None:
@@ -105,7 +111,7 @@ class ZhihuSpider:
             for location in locations:
                 item.locations.append(location.get('name'))
         logging.info('Succeed in scraping info of zhihu user: %s.' % user)
-        self.info = item
+        self.scraped_infos[user] = item
         return item
 
     def scrape_follows(self, user=None, number=0):
@@ -145,8 +151,7 @@ class ZhihuSpider:
             item = self.scrape_info(user=url_token)
             follows.append(item)
         logging.info('Succeed in scraping follows of zhihu user: %s.' % user)
-        self.follows = follows
-        self.follows_user = user
+        self.scraped_follows[user] = follows
         return follows
 
     def scrape_followers(self, user=None, number=0):
@@ -186,16 +191,16 @@ class ZhihuSpider:
             item = self.scrape_info(user=url_token)
             followers.append(item)
         logging.info('Succeed in scraping followers of zhihu user: %s.' % user)
-        self.followers = followers
-        self.followers_user = user
+        self.scraped_followers[user] = followers
         return followers
 
     def scrape_question_by_id(self, id=0):
         if id == 0:
             return None
+        logging.info('Scraping question of id: %d...' % id)
         response = requests.get(question_url.format(id=id, include=question_query), headers=headers)
-        print(response.text)
         if response.status_code == 404:
+            logging.warning('404 error. The question doesn\'t exist.')
             return None
         result = response.json()
         item = ZhihuQuestionItem()
@@ -203,7 +208,6 @@ class ZhihuSpider:
         item.title = result.get('title')
         item.create_time = time.ctime(result.get('created'))
         item.update_time = time.ctime(result.get('updated_time'))
-        print('https://www.zhihu.com/question/%d' % id)
         page = requests.get('https://www.zhihu.com/question/%d' % id, headers=headers)
         bs = BeautifulSoup(page.text, 'lxml')
         content_span = bs.find('div', {'class': 'QuestionRichText'}).div.span
@@ -215,29 +219,200 @@ class ZhihuSpider:
         topics = result.get('topics')
         for topic in topics:
             item.topics.append(topic.get('name'))
+        logging.info('Succeed in scraping question of id: %d.' % id)
+        self.scraped_questions[id] = item
         return item
 
     def scrape_questions_by_user(self, user=None, number=0):
-        pass
+        if user is None:
+            return []
+        logging.info('Scraping questions of zhihu user: %s...' % user)
+        response = requests.get(user_questions_url.format(user=user, offset=0, limit=20), headers=headers)
+        if response.status_code == 404:     # 用户不存在或账号被封禁
+            logging.warning('404 error. The user doesn\'t exist or has been blocked.')
+            return []
+        result = response.json()
+        total = result.get('paging').get('totals')
+        if number <= 0:
+            need_count = 10
+        else:
+            need_count = number if number < total else total
+        finish_count = 0
+        question_ids = []
+        for data in result.get('data'):
+            if finish_count >= need_count:
+                break
+            question_ids.append(data.get('id'))
+            finish_count += 1
+        if finish_count < need_count:
+            position = 0
+            while not result.get('paging').get('is_end'):
+                if finish_count >= need_count:
+                    break
+                position += 20
+                next_page = user_questions_url.format(user=user, offset=position, limit=20)
+                result = requests.get(next_page, headers=headers).json()
+                for data in result.get('data'):
+                    if finish_count >= need_count:
+                        break
+                    question_ids.append(data.get('id'))
+                    finish_count += 1
+        questions = []
+        for question_id in question_ids:
+            item = self.scrape_question_by_id(id=question_id)
+            questions.append(item)
+        logging.info('Succeed in scraping questions of zhihu user: %s.' % user)
+        self.scraped_user_questions[user] = questions
+        return questions
 
     def scrape_answer_by_id(self, id=0):
-        pass
+        if id == 0:
+            return None
+        logging.info('Scraping answer of id: %d...' % id)
+        response = requests.get(answer_url.format(id=id, include=answer_query), headers=headers)
+        if response.status_code == 404:
+            logging.warning('404 error. The answer doesn\'t exist.')
+            return None
+        result = response.json()
+        item = ZhihuAnswerItem()
+        item.id = result.get('id')
+        item.author = result.get('author').get('name')
+        item.question_id = result.get('question').get('id')
+        item.create_time = time.ctime(result.get('created_time'))
+        item.update_time = time.ctime(result.get('updated_time'))
+        page = requests.get('https://www.zhihu.com/question/%d/answer/%d' % (item.question_id, id), headers=headers)
+        bs = BeautifulSoup(page.text, 'lxml')
+        content_span = bs.find('div', {'class': 'RichContent'}).div.span
+        content = re.search(r'<span.*?>(.*)</span>', str(content_span)).group(1)
+        item.content = content
+        item.voteup_count = result.get('voteup_count')
+        item.comment_count = result.get('comment_count')
+        logging.info('Succeed in scraping answer of id: %d.' % id)
+        self.scraped_answers[id] = item
+        return item
 
     def scrape_answers_by_question(self, id=0, number=0):
-        pass
+        if id == 0:
+            return []
+        logging.info('Scraping answers of question: %d...' % id)
+        response = requests.get(question_answers_url.format(id=id, offset=0, limit=20), headers=headers)
+        if response.status_code == 404:     # 问题不存在
+            logging.warning('404 error. The question doesn\'t exist.')
+            return []
+        result = response.json()
+        total = result.get('paging').get('totals')
+        if number <= 0:
+            need_count = 10
+        else:
+            need_count = number if number < total else total
+        finish_count = 0
+        answer_ids = []
+        for data in result.get('data'):
+            if finish_count >= need_count:
+                break
+            answer_ids.append(data.get('id'))
+            finish_count += 1
+        if finish_count < need_count:
+            while not result.get('paging').get('is_end'):
+                if finish_count >= need_count:
+                    break
+                next_page = result.get('paging').get('next')
+                result = requests.get(next_page, headers=headers).json()
+                for data in result.get('data'):
+                    if finish_count >= need_count:
+                        break
+                    answer_ids.append(data.get('id'))
+                    finish_count += 1
+        answers = []
+        for answer_id in answer_ids:
+            item = self.scrape_answer_by_id(id=answer_id)
+            answers.append(item)
+        logging.info('Succeed in scraping answers of question: %d.' % id)
+        self.scraped_question_answers[id] = answers
+        return answers
 
-    def scrape_answers_by_user(self, user=0, number=0):
-        pass
+    def scrape_answers_by_user(self, user=None, number=0):
+        if user is None:
+            return []
+        logging.info('Scraping answers of zhihu user: %s...' % user)
+        response = requests.get(user_answers_url.format(user=user, offset=0, limit=20), headers=headers)
+        if response.status_code == 404:     # 用户不存在或账号被封禁
+            logging.warning('404 error. The user doesn\'t exist or has been blocked.')
+            return []
+        result = response.json()
+        total = result.get('paging').get('totals')
+        if number <= 0:
+            need_count = 10
+        else:
+            need_count = number if number < total else total
+        finish_count = 0
+        answer_ids = []
+        for data in result.get('data'):
+            if finish_count >= need_count:
+                break
+            answer_ids.append(data.get('id'))
+            finish_count += 1
+        if finish_count < need_count:
+            position = 0
+            while not result.get('paging').get('is_end'):
+                if finish_count >= need_count:
+                    break
+                position += 20
+                next_page = user_answers_url.format(user=user, offset=position, limit=20)
+                result = requests.get(next_page, headers=headers).json()
+                for data in result.get('data'):
+                    if finish_count >= need_count:
+                        break
+                    answer_ids.append(data.get('id'))
+                    finish_count += 1
+        answers = []
+        for answer_id in answer_ids:
+            item = self.scrape_answer_by_id(id=answer_id)
+            answers.append(item)
+        logging.info('Succeed in scraping answers of zhihu user: %s.' % user)
+        self.scraped_user_answers[user] = answers
+        return answers
 
-    def save_info(self, directory='./products/'):
-        if not isinstance(self.info, ZhihuUserItem):
-            raise SpiderItemError('\'self.info\' is not an instance of ZhihuUserItem!')
-        csv_file = open(directory + str(self.info.name) + '-info.csv', 'w')
+    def save_info(self, user=None, directory='./products/'):
+        if self.scraped_infos == {}:  # 未爬取过任何用户信息
+            logging.warning('Haven\'t scraped info of any zhihu user.')
+            return
+        if user is None:    # 保存所有爬取过的用户信息
+            csv_file = open(directory + 'all-user-info.csv', 'w')
+            writer = csv.writer(csv_file)
+            writer.writerow(('ID', '用户名', '性别', '头像链接', '行业', '一句话描述', '个人介绍', '提问数', '回答数',
+                             '文章数', '被赞同数', '被感谢数', '被收藏数', '关注数', '粉丝数', '关注话题数', '关注专栏数',
+                             '关注问题数', '关注收藏夹数', '教育经历', '职业经历', '居住地'))
+            for name in self.scraped_infos.keys():
+                info = self.scraped_infos.get(name)
+                if not isinstance(info, ZhihuUserItem):
+                    continue
+                if info.gender == 0:
+                    gender = '女'
+                elif info.gender == 1:
+                    gender = '男'
+                else:
+                    gender = '未知'
+                writer.writerow((info.id, info.name, gender, info.avatar_url, info.business, info.headline,
+                                 info.description, info.question_count, info.answer_count, info.article_count,
+                                 info.voteup_count, info.thanked_count, info.favorited_count, info.following_count,
+                                 info.follower_count, info.following_topic_count, info.following_column_count,
+                                 info.following_question_count, info.following_favlist_count,
+                                 '; '.join([str(edu) for edu in info.educations]),
+                                 '; '.join([str(emp) for emp in info.employments]),
+                                 '; '.join([str(loc) for loc in info.locations])))
+            csv_file.close()
+            logging.info('Succeed in saving infos of all scraped zhihu users.')
+            return
+
+        info = self.scraped_infos.get(user)
+        if not isinstance(info, ZhihuUserItem):
+            raise SpiderItemError('\'info\' isn\'t an instance of ZhihuUserItem.')
+        csv_file = open(directory + str(info.name) + '-info.csv', 'w')
         writer = csv.writer(csv_file)
         writer.writerow(('ID', '用户名', '性别', '头像链接', '行业', '一句话描述', '个人介绍', '提问数', '回答数',
                          '文章数', '被赞同数', '被感谢数', '被收藏数', '关注数', '粉丝数', '关注话题数', '关注专栏数',
                          '关注问题数', '关注收藏夹数', '教育经历', '职业经历', '居住地'))
-        info = self.info
         if info.gender == 0:
             gender = '女'
         elif info.gender == 1:
@@ -253,19 +428,21 @@ class ZhihuSpider:
                          '; '.join([str(emp) for emp in info.employments]),
                          '; '.join([str(loc) for loc in info.locations])))
         csv_file.close()
-        logging.info('Succeed in saving info of zhihu user: %s.' % self.info.name)
+        logging.info('Succeed in saving info of zhihu user: %s.' % info.name)
 
-    def save_follows(self, directory='./products/'):
-        if not isinstance(self.follows, list):
-            raise SpiderItemError('\'self.follows\' is not a list!')
-        if self.follows_user is None:
-            raise SpiderItemError('Haven\'t scraped follows of any zhihu user!')
-        csv_file = open(directory + str(self.follows_user) + '-follows.csv', 'w')
+    def save_user_follows(self, user, directory='./products/'):
+        if self.scraped_follows == {}:
+            logging.warning('Haven\'t scraped follows of any zhihu user.')
+            return
+        infos = self.scraped_follows.get(user)
+        if not isinstance(infos, list):
+            raise SpiderItemError('Haven\'t scraped follows of zhihu user: %s' % user)
+        csv_file = open(directory + str(user) + '-follows.csv', 'w')
         writer = csv.writer(csv_file)
         writer.writerow(('ID', '用户名', '性别', '头像链接', '行业', '一句话描述', '个人介绍', '提问数', '回答数',
                          '文章数', '被赞同数', '被感谢数', '被收藏数', '关注数', '粉丝数', '关注话题数', '关注专栏数',
                          '关注问题数', '关注收藏夹数', '教育经历', '职业经历', '居住地'))
-        for info in self.follows:
+        for info in infos:
             if not isinstance(info, ZhihuUserItem):
                 continue
             if info.gender == 0:
@@ -283,19 +460,21 @@ class ZhihuSpider:
                              '; '.join([str(emp) for emp in info.employments]),
                              '; '.join([str(loc) for loc in info.locations])))
         csv_file.close()
-        logging.info('Succeed in saving follows of zhihu user: %s.' % self.follows_user)
+        logging.info('Succeed in saving follows of zhihu user: %s.' % user)
 
-    def save_followers(self, directory='./products/'):
-        if not isinstance(self.followers, list):
-            raise SpiderItemError('\'self.followers\' is not a list!')
-        if self.followers_user is None:
-            raise SpiderItemError('Haven\'t scraped followers of any zhihu user!')
-        csv_file = open(directory + str(self.followers_user) + '-followers.csv', 'w')
+    def save_user_followers(self, user, directory='./products/'):
+        if self.scraped_followers == {}:
+            logging.warning('Haven\'t scraped followers of any zhihu user.')
+            return
+        infos = self.scraped_followers.get(user)
+        if not isinstance(infos, list):
+            raise SpiderItemError('Haven\'t scraped followers of zhihu user: %s' % user)
+        csv_file = open(directory + str(user) + '-followers.csv', 'w')
         writer = csv.writer(csv_file)
         writer.writerow(('ID', '用户名', '性别', '头像链接', '行业', '一句话描述', '个人介绍', '提问数', '回答数',
                          '文章数', '被赞同数', '被感谢数', '被收藏数', '关注数', '粉丝数', '关注话题数', '关注专栏数',
                          '关注问题数', '关注收藏夹数', '教育经历', '职业经历', '居住地'))
-        for info in self.followers:
+        for info in infos:
             if not isinstance(info, ZhihuUserItem):
                 continue
             if info.gender == 0:
@@ -313,7 +492,7 @@ class ZhihuSpider:
                              '; '.join([str(emp) for emp in info.employments]),
                              '; '.join([str(loc) for loc in info.locations])))
         csv_file.close()
-        logging.info('Succeed in saving followers of zhihu user: %s.' % self.followers_user)
+        logging.info('Succeed in saving followers of zhihu user: %s.' % user)
 
 
 if __name__ == '__main__':
@@ -321,13 +500,19 @@ if __name__ == '__main__':
     info = spider.scrape_info(user='qing-shen-jue-qian')
     print(info)
     spider.save_info()
+
     follows = spider.scrape_follows(user='qing-shen-jue-qian', number=1)
     for follow in follows:
         print(follow)
-    spider.save_follows()
+    spider.save_user_follows('qing-shen-jue-qian')
+
     followers = spider.scrape_followers(user='qing-shen-jue-qian', number=1)
     for follower in followers:
         print(follower)
-    spider.save_followers()
+    spider.save_user_followers('qing-shen-jue-qian')
+
     question = spider.scrape_question_by_id(67684166)
     print(question)
+
+    answer = spider.scrape_answer_by_id(255562932)
+    print(answer)
