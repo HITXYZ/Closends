0126965018384a1deaf5d5d9e81d010e790c8f9b
bluestyle97 <690147660@qq.com>
2017-11-08 16:08:46 +0800

    :rocket:重构微博爬虫，从移动端爬取

--- weibo/items.py
+++ weibo/items.py
@@ -3,10 +3,10 @@
     @date: 2017/10/23
     @desc: Items of weibo scraping
 """
-from baseitem import ScrapeItem
+from base_item import SocialMediaItem
 
 
-class WeiboItem(ScrapeItem):
+class WeiboItem(SocialMediaItem):
     pass
 
 
@@ -14,25 +14,27 @@ class WeiboUserItem(WeiboItem):
     def __init__(self):
         self.id = None
         self.name = None
-        self.sex = None
-        self.address = None
-        self.birthday = None
-        self.synopsis = None
-        self.weibo_number = 0
-        self.follow_number = 0
-        self.fans_number = 0
+        self.gender = None
+        self.avatar_url = None
+        self.location = None
+        self.description = None
+        self.signup_time = None
+        self.weibo_count = 0
+        self.follow_count = 0
+        self.fans_count = 0
 
     def __str__(self):
         string = ''
         string += 'ID: ' + str(self.id) + '\n'
         string += 'Name: ' + str(self.name) + '\n'
-        string += 'Sex: ' + str(self.sex) + '\n'
-        string += 'Address: ' + str(self.address) + '\n'
-        string += 'Birthday: ' + str(self.birthday) + '\n'
-        string += 'Synopsis: ' + str(self.synopsis) + '\n'
-        string += 'Weibo Number: ' + str(self.weibo_number) + '\n'
-        string += 'Follow Number: ' + str(self.follow_number) + '\n'
-        string += 'Fans Number: ' + str(self.follow_number) + '\n'
+        string += 'Gender: ' + str(self.gender) + '\n'
+        string += 'Avatar Url: ' + str(self.avatar_url) + '\n'
+        string += 'Location: ' + str(self.location) + '\n'
+        string += 'Description: ' + str(self.description) + '\n'
+        string += 'Sign-up Time: ' + str(self.signup_time) + '\n'
+        string += 'Weibo Count: ' + str(self.weibo_count) + '\n'
+        string += 'Following Count: ' + str(self.follow_count) + '\n'
+        string += 'Fans Count: ' + str(self.fans_count) + '\n'
         return string
 
     def __hash__(self):
@@ -41,33 +43,44 @@ class WeiboUserItem(WeiboItem):
 
 class WeiboContentItem(WeiboItem):
     def __init__(self):
-        self.owner = None
+        self.id = None
+        self.owner = 0
         self.time = None
         self.content = None
-        self.images = []
+        self.source = None
+        self.pictures = []
 
     def __str__(self):
         string = ''
+        string += 'ID: ' + str(self.id) + '\n'
         string += 'Owner: ' + str(self.owner) + '\n'
         string += 'Time: ' + str(self.time) + '\n'
         string += 'Content: ' + str(self.content) + '\n'
+        string += 'Source: ' + str(self.source) + '\n'
+        string += 'Pictures: ' + '; '.join(str(pic) for pic in self.pictures) + '\n'
         return string
 
     def __hash__(self):
-        return hash(self.content)
+        return hash(self.id)
 
 
 class WeiboRepostContentItem(WeiboContentItem):
     def __init__(self):
         WeiboContentItem.__init__(self)
-        self.repost_source = None
-        self.repost_reason = None
+        self.source_id = None
+        self.source_owner = 0
+        self.source_time = None
+        self.source_content = None
+        self.source_pictures = []
 
     def __str__(self):
         string = WeiboContentItem.__str__(self)
-        string += 'Repost Source: ' + str(self.repost_source) + '\n'
-        string += 'Repost Reason: ' + str(self.repost_reason) + '\n'
+        string += 'Source ID: ' + str(self.source_id) + '\n'
+        string += 'Source Owner: ' + str(self.source_owner) + '\n'
+        string += 'Source Time: ' + str(self.source_time) + '\n'
+        string += 'Source Content: ' + str(self.source_content) + '\n'
+        string += 'Source Pictures: ' + '; '.join([str(pic) for pic in self.source_pictures])
         return string
 
     def __hash__(self):
-        return hash(str(self.content) + str(self.repost_reason))
+        return hash(self.id)
